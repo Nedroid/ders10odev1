b1870891115c08daf904ec71ea6bdf69133474cb
nese <nesekrmn02@gmail.com>
2024-07-08 17:46:35 +0300

    ders10odev1 yapıldı

---
+++ ConsoleApp1/Business/Concrete/BrandManager.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp1.Business.Abstract;
+using ConsoleApp1.Business.Constants;
+using ConsoleApp1.DataAccess.Abstract;
+using ConsoleApp1.DataAccess.Concrete;
+using Core.Utilities.Results;
+using RecapProjectCar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _ıBrandDal;
+
+        public BrandManager(IBrandDal ıBrandDal)
+        {
+            _ıBrandDal = ıBrandDal;
+        }
+
+        public IResult Add(Brand brand)
+        {
+           _ıBrandDal.Add(brand);
+            return new Result(true,Messages.BrandAdded);
+        }
+
+        public IResult Delete(Brand brand)
+        {
+            _ıBrandDal.Delete(brand);
+            return new Result(true,Messages.BrandDeleted);
+        }
+
+        public IDataResult<List<Brand>> GetAll()
+        {
+           _ıBrandDal.GetAll();
+            return new SuccessDataResult<List<Brand>>(_ıBrandDal.GetAll(), Messages.ColorListed); ; 
+        }
+
+        public IResult Update(Brand brand)
+        {
+           _ıBrandDal.Update(brand);
+            return new Result(true,Messages.BrandUpdated);
+        }
+    }
+}
---
+++ ConsoleApp1/Business/Constants/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Business.Constants
+{
+    public static  class Messages
+    {
+        public static string CarAdded = "Araba  eklendi";
+        public static string ColorAdded = "Renk   eklendi";
+        public static string ColorUpdated = "Renk   güncellendi";
+        public static string ColorDeleted = "Renk   silindi";
+        public static string BrandAdded = "Marka   eklendi";
+        public static string BrandUpdated = "Marka   güncellendi";
+        public static string BrandDeleted = "Marka   silindi";
+        public static string BrandListed = "Markalar listelendi";
+        public static string CarInvalidModel = "araba 1990 model den yukarı olmalı";
+        public static string CarListed = "Arabalar listelendi";
+        public static string ColorListed = "Renkler listelendi";
+
+    }
+}
